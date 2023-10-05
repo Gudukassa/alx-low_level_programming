670a1b0db3c394a115f546ded514af780a949eba
Dagmawi Zekarias <dagmzeku@gmail.com>
2023-10-05 05:16:38 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints the size of various computer types
+ * Return 0 (Success)
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+ptintf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+ptintf("Size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+ptintf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+ptintf("Size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+ptintf("Size of a a float: %lu bytes(s)\n", (unsigned long)sizeof(e));
+return (o);
+}
